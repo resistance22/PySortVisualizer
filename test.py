3b4e1cfd39657017db84b5820f374b7bcf2cea71
amin <aminforoutan7@gmail.com>
2020-03-22 19:08:53 +0430

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class SortVisualizer:
+    def __init__(self,master):
+        
+
+
+
+def main():
+    root = Tk()
+
+    root.mainloop()
+
+if __name__ == "__main__": main()

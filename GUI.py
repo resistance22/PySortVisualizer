6fce6dacd7ba31feb0aa45071c3a07b3a764e8fd
amin <aminforoutan7@gmail.com>
2020-03-24 01:45:00 +0430

    adding the comment on L16

--- GUI.py
+++ GUI.py
@@ -10,15 +10,15 @@ class SortVisualizer:
             "insertion",
             "heap"
         ]
-        options = ttk.LabelFrame(master, text="Options", padding=5)
+        options = ttk.LabelFrame(master,width=800, height=200, text="Options", padding=5)
         options.pack()
         self._sort = StringVar()
-
+        #The array size variable
         ttk.Label(options, text='Size:').grid(row=0, column=0)
         self._sizeEntry = ttk.Entry(options, width=20).grid(row=1, column=0, padx=50)
-
+        
         ttk.Label(options, text="Algorithm:").grid(row=0, column=1)
-        self._sortingMenu = ttk.OptionMenu(options, self._sort, *sorting_options).grid(row=1, column=1,padx=50)
+        self._sortingMenu = ttk.OptionMenu(options, self._sort, *sorting_options).grid(row=1, column=1,padx=50,)
 
         startButton = ttk.Button(options, text="Sort")
         startButton.grid(row=0, column=2, rowspan=2,padx=50)

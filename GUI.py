b50eb67e763ed96e198c586f1b2d281983bd8dcb
amin <aminforoutan7@gmail.com>
2020-03-25 13:23:01 +0430

    creating validation for size entry[check desc] -the input must be and integer between 2 and 50

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-
+from bubble_sort_anim import Bubble_sort_anim
 
 class SortVisualizer:
     def __init__(self, master):
@@ -14,15 +14,26 @@ class SortVisualizer:
         options.pack()
         self._sort = StringVar()
         #The array size variable
-        ttk.Label(options, text='Size:').grid(row=0, column=0)
-        self._sizeEntry = ttk.Entry(options, width=20).grid(row=1, column=0, padx=50)
-        
+        ttk.Label(options, text='Size(2<integer<50):').grid(row=0, column=0)
+        #registering the validator function
+        validate = options.register(self.size_valiator)
+
+        self._sizeEntry = ttk.Entry(options, width=20, validate="key", validatecommand=(validate,"%P"))
+        self._sizeEntry.grid(row=1, column=0, padx=50)
         ttk.Label(options, text="Algorithm:").grid(row=0, column=1)
         self._sortingMenu = ttk.OptionMenu(options, self._sort, *sorting_options).grid(row=1, column=1,padx=50,)
 
-        startButton = ttk.Button(options, text="Sort")
+        startButton = ttk.Button(options, text="Sort",command=self.button_command)
         startButton.grid(row=0, column=2, rowspan=2,padx=50)
 
+    def size_valiator(self,inpt):
+        return inpt.isdigit() and int(inpt) > 2 and int(inpt) < 51 or inpt == ""
+    def button_command(self):
+        input = self._sizeEntry.get()
+        if input is not "":
+            size = int(input)
+            anim = Bubble_sort_anim(size)
+            anim.start_anim()
 
 def main():
     root = Tk()

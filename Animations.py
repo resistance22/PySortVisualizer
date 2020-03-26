87a01f012f7b6003f35fb130e43271b371b71d10
amin <aminforoutan7@gmail.com>
2020-03-26 23:05:35 +0430

    implementing the selectoin_sort animation

--- Animations.py
+++ Animations.py
@@ -79,13 +79,11 @@ class Insertion_sort_anim:
 
     def start_anim(self):
         #Starting and then showing the plot animation
-        print(self._y)
         self._anim = FuncAnimation(self._fig, self.animate,frames=self.frame_generator, interval=self._interval,repeat=True)
         plt.show()
 
 
     def stop_anim(self):
-        print('stop')
         self._anim.event_source.stop()
 
     def restart_anime(self):
@@ -93,7 +91,6 @@ class Insertion_sort_anim:
 
 
     def animate(self,i):
-        print(i)
         for p in self._x:
             self._plot[p].set_color('b')
         for p in range(self._frame):
@@ -123,8 +120,77 @@ class Insertion_sort_anim:
         return self._plot
 
 
+
+
+
+
+
+
+class Selection_sort_anim:
+    def __init__(self,max,interval=200):
+        self._max = max
+        self._fig = plt.figure()
+        self._x = [x for x in range(0,max)]
+        self._y = np.random.randint(0,max*4,max)
+        self._plot =  plt.bar(self._x,self._y)
+        self._frame = 0
+        self._index = 0
+        self._interval = interval
+        self._anim = None
+        #making The window appear fullScreen
+        figManager = plt.get_current_fig_manager()
+        figManager.window.showMaximized()
+
+
+
+    def frame_generator(self):
+        for f in range(self._frame,self._max):
+            yield f
+            f += 1
+
+    def start_anim(self):
+        #Starting and then showing the plot animation
+        self._anim = FuncAnimation(self._fig, self.animate,frames=self.frame_generator, interval=self._interval,repeat=True)
+        plt.show()
+
+    def stop_anim(self):
+        self._anim.event_source.stop()
+
+    def animate(self,i):
+        for p in self._x:
+            self._plot[p].set_color('b')
+
+        for p in range(0,self._frame):
+            self._plot[p].set_color('g')
+        self._plot[self._index].set_color('r')
+        self._plot[i].set_color('y')
+        if self._frame == self._max - 1:
+            for p in self._x:
+                self._plot[p].set_color('g')
+            self.stop_anim()
+            return self._plot
+
+        if self._y[self._index] > self._y[i]:
+            self._plot[self._index].set_color('b')
+            self._index = i
+            self._plot[i].set_color('r')
+
+        if i == self._max - 1:
+            self._y[self._frame],self._y[self._index] = self._y[self._index],self._y[self._frame]
+            self._plot[self._frame].set_height(self._y[self._frame])
+            self._plot[self._index].set_height(self._y[self._index])
+
+            self._frame += 1
+            self._index = self._frame
+
+        return self._plot
+
+
+
+
+
 def main():
-    anim1 = Bubble_sort_anim(50,50)
+    anim1 = Selection_sort_anim(10,500)
     anim1.start_anim()
 
 if __name__ == "__main__" : main()
